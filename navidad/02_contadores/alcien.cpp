696f397d5599604e214b03e240f244ac42edd4ea
alexgcr33 <alexgcr33@gmail.com>
2017-02-15 09:18:18 +0100

    ejercicio contador

---
+++ navidad/02_contadores/alcien.cpp
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+printf("Necesito que me digas los numeros del 1 al 100 pedrito \n");
+
+
+printf("Y pedrito que su padre era programador va y le dice a la profe \n");
+
+printf("Pues profe yo como soy muy vago mejor hago esto.. : \n");
+
+for ( int i=1; i<101; i++){
+
+printf("Aqui estan profe %d \n", i);
+};
+
+return EXIT_SUCCESS;
+
+
+}
