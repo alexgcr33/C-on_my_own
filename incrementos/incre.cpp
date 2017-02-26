6e6a687a37cb8ddf606052f629aaa66436feb9f3
alexgcr33 <alexgcr33@gmail.com>
2017-02-26 23:40:14 +0100

    incrementos sencillos

---
+++ incrementos/incre.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int x=15, y=-10 , z=2600520;
+
+x= ++x;
+y= --y;
+z= ++z;
+
+
+printf("pues bueno veamos como va esto.. %i, seg %i y ter es %i... \n", x, y,z);
+return EXIT_SUCCESS;
+
+
+}
---
+++ incrementos/increm.cpp
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int a=5, b=0, c=0, d=0;
+
+b= ++a;
+c= a++;
+
+printf("las cosas son asi a vale %i, b vale %i y c vale %i.. \n", a,b,c);
+
+
+
+return EXIT_SUCCESS;
+
+
+}
